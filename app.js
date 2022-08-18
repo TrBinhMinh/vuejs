672921aa9bf0760bafcb898ec543e78413b016a6
LDAP\tbminh <tbminh@cmcglobal.vn>
2022-08-18 16:13:39 +0700

    :art: update to lesson 47

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
 const app = Vue.createApp({
   data() {
     return {
-      boxASelected: false,
-      boxBSelected: false,
-      boxCSelected: false,
+      goals: [],
+      enteredGoalValue: "",
     };
   },
   methods: {
-    boxSelected(box) {
-      if (box === "A") this.boxASelected = !this.boxASelected;
-      else if (box === "B") this.boxBSelected = !this.boxBSelected;
-      else this.boxCSelected = !this.boxCSelected;
+    addGoal() {
+      this.goals.push(this.enteredGoalValue);
+      this.enteredGoalValue = "";
     },
-  },
-  computed: {
-    boxAClasses() {
-      return { active: this.boxBSelected };
+    removeGoal(index) {
+      this.goals.splice(index, 1);
     },
   },
 });
 
-app.mount("#styling");
+app.mount("#user-goals");

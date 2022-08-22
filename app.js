be26a3abfd79ba5b0ba68bdfe55b093c4c8ff9bd
Binh Minh Tran <minh.tb@gif.vn>
2022-08-22 22:42:05 +0700

    :rocket: initialize component project

--- app.js
+++ app.js
@@ -1,65 +1,28 @@
 const app = Vue.createApp({
   data() {
     return {
-      currentUserInput: "",
-      message: "Vue is great!",
+      detailsAreVisible: false,
+      friends: [
+        {
+          id: "vu",
+          name: "Vu Ta",
+          phone: "0869215538",
+          email: "vuta@gmail.com",
+        },
+        {
+          id: "thinh",
+          name: "Thinh beat",
+          phone: "0936665597",
+          email: "thinhbeat@gmail.com",
+        },
+      ],
     };
   },
   methods: {
-    saveInput(event) {
-      this.currentUserInput = event.target.value;
+    toggleDetails(id) {
+      this.detailsAreVisible = !this.detailsAreVisible;
     },
-    setText() {
-      this.message = this.$refs.userInput.value;
-      this.$refs.userInput.value = "";
-    },
-  },
-  beforeCreate() {
-    console.log("beforeCreate");
-  },
-  created() {
-    console.log("created");
-  },
-  beforeMount() {
-    console.log("beforeMount");
-  },
-  mounted() {
-    console.log("mounted");
-  },
-  beforeUpdate() {
-    console.log("beforeUpdate");
-  },
-  updated() {
-    console.log("update");
-  },
-  beforeUnmount() {
-    console.log("beforeUnmount");
-  },
-  unmounted() {
-    console.log("unmounted");
   },
 });
 
 app.mount("#app");
-
-//.......
-
-const data = {
-  message: "Hello!",
-  longMessage: "Hello! World!",
-};
-
-const handler = {
-  set(target, key, value) {
-    if (key === "message") {
-      target.longMessage = value + "World";
-    }
-    target.message = value;
-  },
-};
-
-const proxy = new Proxy(data, handler);
-
-proxy.message = "Hello!!!!";
-
-// console.log(proxy.longMessage);

c3d8c9f9099ab11bc0122d2aad71b046e5c1bb93
LDAP\tbminh <tbminh@cmcglobal.vn>
2022-08-23 09:44:02 +0700

    :art: update to lesson 71

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const app = Vue.createApp({
   data() {
     return {
-      detailsAreVisible: false,
       friends: [
         {
           id: "vu",
@@ -20,6 +19,37 @@ const app = Vue.createApp({
   },
   methods: {
     toggleDetails(id) {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+});
+
+app.component("friend-contact", {
+  template:
+    /*html*/
+    `
+    <li>
+      <h2>{{ friend.name }}</h2>
+      <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+      <ul v-show="detailsAreVisible">
+        <li><strong>Phone:</strong> {{ friend.phone }}</li>
+        <li><strong>Email:</strong> {{ friend.email }}</li>
+      </ul>
+    </li>
+  `,
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "vu",
+        name: "Vu Ta",
+        phone: "0869215538",
+        email: "vuta@gmail.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
       this.detailsAreVisible = !this.detailsAreVisible;
     },
   },

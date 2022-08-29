6571a532d9c82ed9ff1cc4a9933e3f747a739c53
LDAP\tbminh <tbminh@cmcglobal.vn>
2022-08-29 09:14:53 +0700

    :rocket: initialize the advance component project

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 import { createApp } from "vue";
+
 import App from "./App.vue";
-import FriendContact from "./components/FriendContact.vue";
-import AddFriend from "./components/AddFriend.vue";
+import BaseBadge from "./components/BaseBadge.vue";
 
 const app = createApp(App);
 
-app.component("friend-contact", FriendContact);
-app.component("add-friend", AddFriend);
+app.component("base-badge", BaseBadge);
 
 app.mount("#app");

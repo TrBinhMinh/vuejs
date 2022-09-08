aa65fc20b0bec3c44d9b151f5d9734cd2479f67b
LDAP\tbminh <tbminh@cmcglobal.vn>
2022-09-08 17:30:29 +0700

    :art: update to lesson 159

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -7,7 +7,8 @@
           >Load Submitted Experiences</base-button
         >
       </div>
-      <ul>
+      <p v-if="isLoading">Loading...</p>
+      <ul v-else>
         <survey-result
           v-for="result in results"
           :key="result.id"
@@ -29,10 +30,12 @@ export default {
   data() {
     return {
       results: [],
+      isLoading: false,
     };
   },
   methods: {
     loadExperiences() {
+      this.isLoading = true;
       fetch(
         "https://vue-http-demo-f85da-default-rtdb.firebaseio.com/surveys.json"
       )
@@ -42,6 +45,7 @@ export default {
           }
         })
         .then((data) => {
+          this.isLoading = false;
           const results = [];
           for (const id in data) {
             results.push({
@@ -54,6 +58,9 @@ export default {
           this.results = results;
         });
     },
+  },
+  mounted() {
+    this.loadExperiences();
   },
 };
 </script>
